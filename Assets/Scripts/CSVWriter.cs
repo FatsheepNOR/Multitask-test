58c2654f011e3470880ff3a57d985db434dd1fcf
FatsheepNOR <fatsheep200@gmail.com>
2019-01-02 21:53:41 +0100

    Code start
    started writing functionality

---
+++ Assets/Scripts/CSVWriter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+using System;
+
+public class CSVWriter : MonoBehaviour {
+
+    public string CSVPath = "/TestData/";
+
+
+	public void writeLineToCSV (string fileName, string content)
+    {
+        string filePath = CSVPath + fileName;
+        StreamWriter writer = new StreamWriter(filePath);
+        writer.WriteLine(content);
+        writer.Flush();
+        writer.Close();
+    }
+}
+
---
+++ Assets/Scripts/DataCollector.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataCollector : MonoBehaviour {
+
+    public int NumTries = 20;
+    public float interval = 1.0f;
+    public GameObject[] arrows;
+    public CSVWriter cW;
+    public Text personNumber;
+
+    private int currPerson = 0;
+    private bool isTesting = false;
+    private bool isChoosing = false;
+
+    private 
+    private float choiceTime = 0.0f;
+
+    private float timeStamp = 0.0f;
+
+    string csvTitles = "ArrowType,Result,Time";
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (isChoosing)
+        {
+            choiceTime += Time.deltaTime;
+        }
+       
+	}
+
+    public void StartTest ()
+    {
+        cW.writeLineToCSV(currPerson + ".csv", csvTitles);
+        timeStamp = Time.time + interval;
+        isTesting = true;
+    }
+
+    void NextArrow ()
+    {
+
+
+    }
+
+    public void NextPerson ()
+    {
+        currPerson++;
+        personNumber.text = "#" + currPerson;
+    }
+}
