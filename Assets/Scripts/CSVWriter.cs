34cb4a238aa094bf06b1d48768caa3b8fd7410a8
FatsheepNOR <fatsheep200@gmail.com>
2019-01-11 00:41:23 +0100

    Finish!
    Program is ccomplete with all functionality and has been rolled out to customer. :^)

--- Assets/Scripts/CSVWriter.cs
+++ Assets/Scripts/CSVWriter.cs
@@ -12,17 +12,8 @@ public class CSVWriter : MonoBehaviour {
 
 	public void writeLineToCSV (string fileName, string content)
   {
-    Application.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("nb-NO");
     string filePath = Application.dataPath + CSVPath + fileName;
-        /*
-        FileStream file = File.Open(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
-        StreamWriter writer = new StreamWriter(file);
-        writer.WriteLine(content);
-        file.Flush();
-        file.Close();
-        writer.Flush();
-        writer.Close();
-        */
+
     StreamWriter sr;
     if (File.Exists(filePath))
     {
--- Assets/Scripts/DataCollector.cs
+++ Assets/Scripts/DataCollector.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 
 public class DataCollector : MonoBehaviour {
 
-    public int NumTries = 20;
-    public float interval = 1.0f;
+    public InputField NumTries;
+    public InputField interval;
     public GameObject[] arrows;
     public CSVWriter cW;
-    public Text personNumber;
+    public Text personNumber; 
+    public GameObject gameScreen;
+    public GameObject endScreen;
 
     private int currPerson = 0;
+    private int currChoice = 0;
     private bool isTesting = false;
     private bool isChoosing = false;
 
@@ -45,35 +48,58 @@ public class DataCollector : MonoBehaviour {
     public void StartTest ()
     {
         cW.writeLineToCSV(currPerson + ".csv", csvTitles);
-        timeStamp = Time.time + interval;
+        timeStamp = Time.time + float.Parse(interval.text);
+        currChoice = 0;
         isTesting = true;
     }
 
     void NextArrow ()
     {
-      correctArrow = arrows[Random.Range(0,arrows.Length)];
-      correctArrow.SetActive(true);
-      choiceTime = 0;
-      isChoosing = true;
+        if (currChoice >= int.Parse(NumTries.text))
+        {
+            EndTest();
+        }
+
+        correctArrow = arrows[Random.Range(0,arrows.Length)];
+        correctArrow.SetActive(true);
+        choiceTime = 0;
+        isChoosing = true;
     }
 
     public void MakeChoice (string dir)
     {
-      isChoosing = false;
-      bool isCorrect = false;
-      if (correctArrow.name[0] == dir[0])
-      {
-        isCorrect = true;
-      }
-      correctArrow.SetActive(false);
-      string resultData = correctArrow.name + ";" + isCorrect + ";" + choiceTime;
-      cW.writeLineToCSV(currPerson + ".csv", resultData);
-      timeStamp = Time.time + interval;
+        if (isChoosing)
+        {
+            isChoosing = false;
+            bool isCorrect = false;
+            if (correctArrow.name[0] == dir[0])
+            {
+                isCorrect = true;
+            }
+            correctArrow.SetActive(false);
+            string resultData = correctArrow.name + ";" + isCorrect + ";" + choiceTime;
+            cW.writeLineToCSV(currPerson + ".csv", resultData);
+            timeStamp = Time.time + float.Parse(interval.text);
+            currChoice++;
+        }
+    }
+
+    public void EndTest ()
+    {
+        isTesting = false;
+        gameScreen.SetActive(false);
+        endScreen.SetActive(true);
     }
 
     public void NextPerson ()
     {
         currPerson++;
+        personNumber.text = "#" + currPerson;
+    }
+
+    public void ResetTest()
+    {
+        currPerson = 0;
         personNumber.text = "#" + currPerson;
     }
 }
